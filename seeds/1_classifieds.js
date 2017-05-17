fd3672cc4cf90d5a25097a27647b373b4f883bfd
rhysgoehring <rhysgoehring@gmail.com>
2017-05-17 11:00:54 -0600

    database seeded

--- seeds/1_classifieds.js
+++ seeds/1_classifieds.js
@@ -1,13 +1,23 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+  return knex('classifieds').del()
+    .then(function() {
+      return Promise.all([
+        knex('classifieds').insert([{
+        id: 1,
+        title: 'NES Classic',
+        description: 'I got lucky and found it, and decided to charge 10x what it was worth.',
+        price: 600,
+        item_image: 'http://www.nintendo.com/images/page/nes-classic/nes-classic-edition-box.png'
+        },
+        {
+        id: 2,
+        title: 'Pikachu 9" Plush Stuffed Toy',
+        description: 'Polyester fiber construction Officially licensed.',
+        price: 10,
+        item_image: 'https://images-na.ssl-images-amazon.com/images/I/41VwGotRZsL._SY300_.jpg'
+        }])
       ]);
+    }).then(() => {
+      return knex.raw("select setval('classifieds_id_seq', (select max(id) from classifieds));")
     });
 };
