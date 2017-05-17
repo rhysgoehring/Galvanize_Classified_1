5ced96b4a88169e72ea5e1afd70cd733234f35ac
rhysgoehring <rhysgoehring@gmail.com>
2017-05-17 16:04:36 -0600

    didn't runtests during DB creation, modified schema.

--- migrations/20170517102150_classifieds.js
+++ migrations/20170517102150_classifieds.js
@@ -2,11 +2,11 @@
 
 exports.up = function(knex) {
   return knex.schema.createTable('classifieds', (table) => {
-    table.increments('id').primary().notNullable();
-    table.text('title', 255).notNullable();
-    table.text('description', 255).notNullable();
-    table.integer('price').notNullable();
-    table.text('item_image').notNullable();
+    table.increments('id').primary();
+    table.string('title', 255).notNullable();
+    table.string('description', 255).notNullable();
+    table.decimal('price').notNullable();
+    table.string('item_image', 255).notNullable();
     table.timestamps(true, true);
   });
 };
