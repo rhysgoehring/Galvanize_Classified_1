b454ab7bce46f835663e48e916f254d58e47798f
rhysgoehring <rhysgoehring@gmail.com>
2017-05-17 16:53:46 -0600

    rewrote routes so they pass tests, postman was giving me the responses i wanted before except on update/del

--- routes/classifieds.js
+++ routes/classifieds.js
@@ -5,34 +5,35 @@ const knex = require('../knex')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  knex('classifieds').select('id','title','description','price','item_image').then((classifieds) => {
+  knex('classifieds').select('id', 'title', 'description', 'price', 'item_image').then((classifieds) => {
       res.send(classifieds);
     })
     .catch((err) => {
       next(err);
     })
-})
+});
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id
   console.log('id is    ', id)
   knex('classifieds').where('id', id)
     .select(['id', 'title', 'description', 'price', 'item_image']).then((classifieds) => {
-      res.send(classifieds[0])
+      res.send(classifieds[0]);
     })
-})
+    .catch((err) => {
+      next(err);
+    })
+});
 
 router.post('/', (req, res, next) => {
-  let newEntry = {
-    'id': req.body.id,
-    'title': req.body.title,
-    'description': req.body.description,
-    'price': req.body.price,
-    'item_image': req.body.item_image,
-    'created at': new Date()
-  }
-  knex('classifieds').insert(newEntry).returning("*").then((response) => {
-      res.send(response[0])
+  knex('classifieds').returning(['id', 'title', 'description', 'price', 'item_image'])
+    .insert({
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      item_image: req.body.item_image
+    }).then((classifieds) => {
+      res.send(classifieds[0]);
     })
     .catch((err) => {
       next(err);
@@ -40,33 +41,31 @@ router.post('/', (req, res, next) => {
 });
 
 router.patch('/:id', (req, res, next) => {
-  let updatedEntry = {
-    'id': req.body.id,
-    'title': req.body.title,
-    'description': req.body.description,
-    'price': req.body.price,
-    'item_image': req.body.item_image,
-    'created_at': new Date()
-  }
-  knex('classifieds').update(updatedEntry).returning("*").then((response) => {
-    res.send(response[0])
-  })
-  .catch((err) => {
-    next(err);
-  })
-});
-
-router.delete('/:id', (req, res, next) => {
-  knex("classifieds").del().where("id", req.params.id),returning("*").then((response) => {
-    let classified = response[0]
-    delete classified.id
-    res.send(classified);
-  })
+  const id = req.params.id
+  knex('classifieds').where('id', id).returning(['id', 'title', 'description', 'price', 'item_image'])
+    .update({
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      item_image: req.body.item_image
+    })
+    .then((classifieds) => {
+      res.send(classifieds[0]);
+    })
+    .catch((err) => {
+      next(err);
+    })
 })
 
-
-
-
-
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  knex('classifieds').where('id', id).returning(['id', 'title', 'description', 'price', 'item_image']).del()
+    .then((classifieds) => {
+      res.send(classifieds[0]);
+    })
+    .catch((err) => {
+      next(err);
+    })
+});
 
 module.exports = router;
