f2211efd402121ce85850e39013b6f4ec6c82848
rhysgoehring <rhysgoehring@gmail.com>
2017-05-17 10:42:13 -0600

    databases set up, table created

--- knexfile.js
+++ knexfile.js
@@ -2,8 +2,17 @@
 
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/classified_dev'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/classified_test'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
 
 };
---
+++ migrations/20170517102150_classifieds.js
@@ -0,0 +1,15 @@
+'use strict'
+
+exports.up = function(knex) {
+  return knex.schema.createTable('classifieds', (table) => {
+    table.increments('id').primary().notNullable();
+    table.text('title', 255).notNullable();
+    table.text('description', 255).notNullable();
+    table.integer('price').notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('classifieds')
+};
